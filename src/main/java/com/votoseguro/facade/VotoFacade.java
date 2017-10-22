1312ea575d2eeb8b5fb0a813a013bb58c61bc5b0
luis <luis@luis-PC>
2017-10-21 20:55:40 -0500

    votar estilo

--- src/main/java/com/votoseguro/facade/VotoFacade.java
+++ src/main/java/com/votoseguro/facade/VotoFacade.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.votoseguro.facade;
+
 import com.votoseguro.entity.Tblcandidato;
 import com.votoseguro.entity.Tblperiodo;
 import com.votoseguro.entity.Tblvotante;
@@ -16,68 +17,83 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+
 /**
  *
  * @author Luis Eduardo Valdez
  */
 @Stateless
-public class VotoFacade extends AbstractFacade<Tblvoto>{
-   @PersistenceContext(unitName = "votoseguroPU")
-     private EntityManager em;   
-    
-    public VotoFacade(){
-    super(Tblvoto.class);
+public class VotoFacade extends AbstractFacade<Tblvoto> {
+
+    @PersistenceContext(unitName = "votoseguroPU")
+    private EntityManager em;
+
+    public VotoFacade() {
+        super(Tblvoto.class);
     }
-    
+
     @Override
-    protected EntityManager getEntityManager(){
-    return em;
-    } 
-    
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
     /*hacer insert que jale el id de periodo
       id de candidato  
       1 / numero de candiatos para valor
       select * from tblperiodo where estadoper = 'PROCESO'
      jalar id del votante
-*/
-    
-    public void votar(List<Tblcandidato> selectedCandidatos,Tblperiodo periodo, Tblvotante votante){
+     */
+    public void votar(List<Tblcandidato> selectedCandidatos, Tblperiodo periodo, Tblvotante votante) {
         int c = 0;
         boolean flag = false;
         List<Tblvoto> voto = new ArrayList<>();
-        Double valor = 1/Double.valueOf(selectedCandidatos.size());
-        String valorS = String.valueOf(valor);
-        for (Tblcandidato candidato : selectedCandidatos) {
-            Tblvoto v = new Tblvoto();
-            v.setIdcandidato(candidato);
-            v.setIdperiodo(periodo);
-            v.setIdvotante(votante);
-            v.setValor(new BigDecimal(valorS));
-            voto.add(v);
-        }
-        for (Tblvoto tblvoto : voto) {
-            try {
-               create(tblvoto);
-               c++;
-            } catch (Exception e) {
-                System.out.println("com.votoseguro.facade.VotoFacade.votar()");
-                e.printStackTrace();
-            }
-            
-        }
-        if (c == selectedCandidatos.size()) {
-            flag = true;
-            System.out.println("se insertaron todos");
-        }
-    
-    }
-    
-    
-    public boolean yaVoto(String idvotante, String idperiodo){
-    boolean flag = true;
-    List<Tblvoto> votos = new ArrayList<>();
+        List<Tblvoto> listaEntity = new ArrayList<>();
+        String sql = "select * from tblvoto where idperiodo = ? and idvotante = ?"; 
+
         try {
-            Query q = em.createNativeQuery("select * from tblvoto where idvotante = ? and idperiodo = ?",Tblvoto.class);
+            Query q = em.createNativeQuery(sql, Tblvoto.class);
+            q.setParameter(1, periodo.getIdperiodo());
+            q.setParameter(2, votante.getIdvotante());
+            listaEntity = q.getResultList();//valida otra vez si ya voto para evitar fraudes
+            if (listaEntity.isEmpty()) {
+
+                Double valor = 1 / Double.valueOf(selectedCandidatos.size());
+                String valorS = String.valueOf(valor);
+                for (Tblcandidato candidato : selectedCandidatos) {
+                    Tblvoto v = new Tblvoto();
+                    v.setIdcandidato(candidato);
+                    v.setIdperiodo(periodo);
+                    v.setIdvotante(votante);
+                    v.setValor(new BigDecimal(valorS));
+                    voto.add(v);
+                }
+                for (Tblvoto tblvoto : voto) {
+
+                    create(tblvoto);
+                    c++;
+
+                }
+                if (c == selectedCandidatos.size()) {
+                    flag = true;
+                    System.out.println("se insertaron todos");
+                }
+
+            } else {
+                System.out.println("tramposo");
+            }
+
+        } catch (Exception e) {
+            System.out.println("com.votoseguro.facade.VotoFacade.votar()");
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean yaVoto(String idvotante, String idperiodo) {
+        boolean flag = true;
+        List<Tblvoto> votos = new ArrayList<>();
+        try {
+            Query q = em.createNativeQuery("select * from tblvoto where idvotante = ? and idperiodo = ?", Tblvoto.class);
             q.setParameter(1, idvotante);
             q.setParameter(2, idperiodo);
             votos = q.getResultList();
@@ -87,13 +103,13 @@ public class VotoFacade extends AbstractFacade<Tblvoto>{
                     flag = true;
                 }
             }
-            
+
         } catch (Exception e) {
             System.out.println("com.votoseguro.facade.VotoFacade.yaVoto()");
             e.printStackTrace();
         }
-    
-    return flag;
-    
+
+        return flag;
+
     }
 }
